f3cb0284a8c9cd6e057d951e4cb8ed5a90aa55a6
SheikhaAlenezi <shei5ha98@gmail.com>
2025-07-28 19:06:54 +0300

    done task1

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,21 @@
+import React from "react";
 import { Image, Text, View } from "react-native";
+
+//import { products } from './products';
+
+interface indexProps {
+  name: string;
+  price: number;
+}
+// const index = ({ name, price}: indexProps) => {
+//   return {
+//     <View style={styles.container}>
+//     <Text> coookie,(name) price=(price )</Text>
+//     </View>
+//   }
+// }
+
+// export default indexProps:
 
 export default function Index() {
   return (
@@ -12,10 +29,15 @@ export default function Index() {
           uri: "https://img.freepik.com/premium-vector/bakery-shop-building-facade-with-signboard-baking-store-cafe-bread-pastry-dessert-shop-showcases-with-various-bread-cakes-products_169241-3309.jpg?w=2000",
         }}
       />
+      {/* <view>
+        {products.map((product,index)=>)
+        <View key ={index}> 
+        <Image 
+        style={{width:100,height:100}}
+      source={{uri: products.image}} />
+      <Text>{products.name}</Text>
+      <Text> ${products.price.toFixed(2) </Text> </view>
+      }} </View> */}
     </View>
   );
 }
-
-// const styles = StyleSheet({
-//   container: {},
-// });
---
+++ data/products.ts
@@ -0,0 +1,13 @@
+export const products = [
+{   name:"chocolate cookie",
+    price:2.500,
+    image: "https://tastesbetterfromscratch.com/wp-content/uploads/2020/02/Double-Chocolate-Cookies24-Thumbnail-1.jpg"
+}
+
+{   name:"white cookie",
+    price:2.500,
+    image:"https://cloudykitchen.com/wp-content/uploads/2023/04/baked-white-chocolate-chip-cookies.jpg"
+}
+
+
+];
